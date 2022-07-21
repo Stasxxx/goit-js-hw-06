d218dd43be2dc4ce6f613979e3895566c0d221a0
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-21 22:51:19 +0300

    task - 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,4 @@
 const categoriesEl = document.querySelectorAll('.item');
-
 console.log('Number of categories', categoriesEl.length);
 
 categoriesEl.forEach(item => {
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsEl = document.querySelector('ul');
+console.log(ingredientsEl);
+
+const makeIngredientsList = ingredients => {
+  return ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList = 'item';
+  itemEl.textContent = ingredient;
+    
+  return itemEl
+});
+}
+
+const listEl = makeIngredientsList(ingredients);
+ingredientsEl.after(...listEl);
+
+
