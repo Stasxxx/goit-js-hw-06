afde5b076913ccc8a4d3591085022af736d60756
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-24 17:48:15 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body')
+const textColor = document.querySelector('.color');
+const btnChangeColor = document.querySelector('.change-color');
+
+// console.log(textColor.textContent = `${getRandomHexColor()}`);
+
+btnChangeColor.addEventListener('click', () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  textColor.textContent = `${getRandomHexColor()}`;
+});
