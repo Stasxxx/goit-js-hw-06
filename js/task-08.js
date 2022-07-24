81f8b827453077075ab5218d0d5a7c581396ac98
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-07-24 17:01:30 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const elements = event.currentTarget.elements;
+    const email = elements.email.value;
+    const password = elements.email.value;
+
+    if (email === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+    }
+    
+    const formData = {
+        email: email,
+        password: password,
+    }
+
+    console.log(formData)
+    event.currentTarget.reset();
+}
 
